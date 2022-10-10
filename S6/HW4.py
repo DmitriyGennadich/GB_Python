6213ea8c3e994501ac154f5bc80fe54607fb3dc6
Dmitriy <fpmt2010@rambler.ru>
2022-10-10 15:34:56 +0300

    add HWS6

---
+++ S6/HW4.py
@@ -0,0 +1,24 @@
+#Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+#Пример:
+#- [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+from functools import reduce
+
+# # было
+# def sum_odd_index(lst):
+#     s = 0
+#     for i in range(len(lst)):
+#         if i % 2 != 0:
+#             s += lst[i]
+#     print(f"Сумма равна: {s}")
+
+# lst = [2, 3, 5, 9, 3]
+# sum_odd_index(lst)
+# lst = list(map(int, input("Введите числа через пробел:\n").split()))
+# sum_odd_index(lst)
+
+
+# стало
+list = list(map(int, input(f'Задайте список через пробел: ').split()))
+list_filtered = list[1::2]
+print(reduce(lambda x, y: x + y, list_filtered))
